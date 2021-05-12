6f6b94eeb4d5d2c690871582d1b827599dbfdda9
Emiliano Fant <emiliano.fant@split.io>
2021-05-12 15:54:45 -0300

    feat: added yargs and execution params to start the cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,21 +1,59 @@
-/* eslint-disable no-process-env */
-import { exit } from 'process';
+import fs from 'fs';
+import { exit, env, argv } from 'process';
 import { synchroniserSettingsValidator } from './settings';
 import { SynchroniserManager } from './manager';
 import { validateApiKey } from '@splitsoftware/splitio-commons/src/utils/inputValidation';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
 console.log('# Synchroniser: Initialization');
+
 /**
  * The API Url to be set for this example.
  */
-const apiUrl = process.env.API_URL;
+let apiUrl: string | undefined;
 /**
  * The API key value.
  */
-const apikey = process.env.APIKEY;
+let apikey: string | undefined;
 
-if (!apiUrl) {
-  console.log('Invalid API URL configuration.');
+const yargv = yargs(hideBin(argv))
+  .usage('Usage: $0 [options]')
+  .command('sync', 'Start synchronising tasks.')
+  .example('$0 -m json --config path2/file.json ', '| Set settings from JSON file.')
+  .example('$0 -m env', '| Set settings from .env file.')
+  .alias('m', 'mode')
+  .nargs('m', 1)
+  .config('configs', function (configPath) {
+    return JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+  })
+  .describe('b', 'Set config mode: json | env')
+  .demandOption(['m'])
+  .help('h')
+  .alias('h', 'help')
+  .epilog('copyright 2021')
+  .argv;
+
+const { mode, APIKEY, API_URL } = yargv;
+
+console.log(`> Synchroniser's configs from: ${mode}`);
+
+switch (mode) {
+  case 'json':
+    apikey = APIKEY as string;
+    apiUrl = API_URL as string;
+    break;
+  case 'env':
+    apikey = env.APIKEY;
+    apiUrl = env.API_URL;
+    break;
+  default:
+    console.log('----');
+    break;
+}
+
+if (!apiUrl || !apikey) {
+  console.log('Unable to initialize Synchroniser task: Invalid or missing settings.');
   exit(0);
 }
 /**
