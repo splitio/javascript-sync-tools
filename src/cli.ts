d44b6af8976a458104919665bdfd8ab545d5f56b
Emiliano Fant <emiliano.fant@split.io>
2021-06-09 11:56:57 -0300

    feat: added APIs set via CLI param, and some fixes/refactor

--- src/cli.ts
+++ src/cli.ts
@@ -13,13 +13,17 @@ dotenv.config();
 console.log('# Synchroniser: Initialization');
 
 /**
- * The API Url to be set for this example.
+ * The API Url .
  */
-let apiUrl: string | undefined;
+let _apiUrl: string | undefined;
+/**
+ * The Events API Url.
+ */
+let _eventsApiUrl: string | undefined;
 /**
  * The API key value.
  */
-let apikey: string | undefined;
+let _apikey: string | undefined;
 let customStorage: ICustomStorageWrapper;
 
 const yargv = yargs(hideBin(argv))
@@ -27,39 +31,56 @@ const yargv = yargs(hideBin(argv))
   .command('sync', 'Start synchronising tasks.')
   .example('$0 -m json --config path2/file.json -s path2/storage.js', '| Set settings from JSON file. -d')
   .example('$0 -m env -s path2/storage.js', '| Set settings from .env file.')
+  .example(
+    '$0 -k {A_VALID_APIKEY} -s path2/storage.js -r https://events.split.io/api/',
+    '| Set APIKEY and Split API URL from param.'
+  )
   .alias('s', 'storage')
   .nargs('s', 1)
   .alias('m', 'mode')
   .nargs('m', 1)
   .alias('d', 'debug')
   .nargs('d', 0)
+  .alias('k', 'apikey')
+  .nargs('k', 1)
+  .alias('r', 'apiUrl')
+  .nargs('r', 1)
+  .alias('e', 'eventsApiUrl')
+  .nargs('e', 1)
   .config('json-file', function (configPath) {
     return JSON.parse(fs.readFileSync(configPath, 'utf-8'));
   })
   .describe('m', 'Set config mode: json | env')
   .describe('s', 'Path to the JS file exposing the Storage API')
   .describe('d', 'Set debug Logger enable')
-  .demandOption(['m', 's'])
+  .describe('k', 'Set the apikey')
+  .describe('r', 'Set the Split API URL')
+  .describe('e', 'Set the Split Events API URL')
+  .demandOption(['s'])
   .help('h')
   .alias('h', 'help')
   .epilog('copyright 2021')
   .argv;
 
-const { mode, storage, APIKEY, API_URL, debug } = yargv;
+const { mode, storage, APIKEY, apikey, apiUrl, API_URL, eventsApiUrl, EVENTS_API_URL, debug } = yargv;
 
-console.log(` > Synchroniser's configs from: ${mode}`);
+console.log(` > Synchroniser's configs from: ${mode || 'CLI params'}`);
 
 switch (mode) {
   case 'json':
-    apikey = APIKEY as string;
-    apiUrl = API_URL as string;
+    _apikey = APIKEY as string;
+    _apiUrl = API_URL as string;
+    _eventsApiUrl = EVENTS_API_URL as string;
     break;
   case 'env':
-    apikey = env.APIKEY;
-    apiUrl = env.API_URL;
+    _apikey = env.APIKEY;
+    _apiUrl = env.API_URL;
+    _eventsApiUrl = env.EVENTS_API_URL;
     break;
   default:
-    console.log('----');
+    _apikey = apikey as string;
+    _apiUrl = apiUrl as string;
+    _eventsApiUrl = eventsApiUrl as string;
     break;
 }
 
@@ -71,22 +92,23 @@ try {
   exit(0);
 }
 
-if (!apiUrl || !apikey) {
-  console.log('Unable to initialize Synchroniser task: Invalid or missing settings.');
+if (!_apikey) {
+  console.log('Unable to initialize Synchroniser task: missing APIKEY.');
   exit(0);
 }
+
 /**
  * Settings creation.
  */
 const settings = synchroniserSettingsValidator({
   core: {
-    authorizationKey: apikey,
+    authorizationKey: _apikey,
   },
   urls: {
     // CDN having all the information for your environment
-    sdk: apiUrl,
+    sdk: _apiUrl,
     // Storage for your SDK events
-    // events: 'https://events.split.io/api',
+    events: _eventsApiUrl,
   },
   storage: {
     type: 'CUSTOM',
@@ -97,8 +119,8 @@ const settings = synchroniserSettingsValidator({
   debug: debug || false,
 });
 
-if (!validateApiKey(settings.log, apikey)) {
-  console.log('Missing APIKEY configuration.');
+if (!validateApiKey(settings.log, _apikey)) {
+  console.log('Unable to initialize Synchroniser task: invalid APIKEY.');
   exit(0);
 }
 
