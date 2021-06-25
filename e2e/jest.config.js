ec8bdb6f68e1dab72626ab1c09979dc7a9c7cf7d
Emiliano Fant <emiliano.fant@split.io>
2021-06-25 18:00:00 -0300

    test: added e2e configuration to support splitio typescript files

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -7,4 +7,10 @@ module.exports = {
   roots: [
     '<rootDir>',
   ],
+  // Test files are .js and .ts files inside of __tests__ folders and with a suffix of .test or .spec
+  testMatch: ['./**/?(*.)+(spec|test).[jt]s'],
+  // Required when using Split dependencies via GitHub
+  // `"@splitsoftware/splitio-commons": "github:splitio/javascript-commons#branch_name",`
+  transform: { '../node_modules/@splitsoftware/.+\\.(j|t)s$': 'ts-jest' },
+  transformIgnorePatterns: ['/node_modules/(?!@splitsoftware/.*)'],
 };
