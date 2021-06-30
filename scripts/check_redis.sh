01fd754e77fa4af012a36392c41335b5a70d790b
Emiliano Fant <emiliano.fant@split.io>
2021-06-30 10:35:02 -0300

    chore: removed unnecesary jest config files & fixed test scripts

--- scripts/check_redis.sh
+++ scripts/check_redis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Check if a Redis instance is up and running.
-if ! ./scripts/wait-for-it.sh localhost:6379 --timeout=5 --strict -- echo "Redis service is up"
+if ! ./scripts/wait_for_it.sh localhost:6379 --timeout=5 --strict -- echo "Redis service is up"
 then
 	echo '>> NO REDIS SERVICE WAS FOUND'
 	exit 1
