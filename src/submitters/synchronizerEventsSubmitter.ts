5b499d57a957d3f16d40173540b16b62effc0e42
Emiliano Fant <emiliano.fant@split.io>
2021-07-07 15:19:13 -0300

    feat: added function to wrap batch processing capability

--- src/submitters/synchronizerEventsSubmitter.ts
+++ src/submitters/synchronizerEventsSubmitter.ts
@@ -9,7 +9,7 @@ import { groupByMetadata, metadataToHeaders } from './metadataUtils';
 /**
  * Constant to define the amount of Events to pop from Storage.
  */
-const EVENTS_AMOUNT = 1000;
+const EVENTS_AMOUNT_DEFAULT = 1000;
 /**
  * Amount of attempts to retry a POST request action.
  */
@@ -49,14 +49,14 @@ function retry(
  * @param {IPostEventsBulk}   postEventsBulk  The Split's HTTPClient API to perform the POST request.
  * @param {IEventsCacheAsync} eventsCache     The Events storage Cache from where to retrieve the Events data.
  * @param {ILogger}           logger          The Synchronizer's Logger.
- * @returns {() => Promise<boolean|string>}
+ * @returns {() => Promise<boolean>}
  */
 export function eventsSubmitterFactory(
   postEventsBulk: IPostEventsBulk,
   eventsCache: IEventsCacheAsync,
   logger: ILogger,
   // @todo: Add retry param.
-): () => Promise<boolean> {
+): (batchSize?: number) => Promise<boolean> {
   /**
    * Function to wrap the POST requests and retries attempt.
    *
@@ -69,40 +69,51 @@ export function eventsSubmitterFactory(
       ATTEMPTS_NUMBER
     );
   }
+  /**
+   * Function to wrap a batch process of events, in order to make it iterative.
+   *
+   * @param {number} batchSize  A configurable amount of events to POP from Storage.
+   * @returns {Promise<boolean>}
+   */
+  function processEventsBatch(batchSize: number = EVENTS_AMOUNT_DEFAULT) {
+    return eventsCache.popNWithMetadata(batchSize)
+      .then(async (events) => {
+        const processedEvents: ProcessedByMetadataEvents = groupByMetadata(events, 'm');
+        const _eMetadataKeys = Object.keys(processedEvents);
 
-  return () => eventsCache.popNWithMetadata(EVENTS_AMOUNT)
-    .then(async (events) => {
-      const processedEvents: ProcessedByMetadataEvents = groupByMetadata(events, 'm');
-      const _eMetadataKeys = Object.keys(processedEvents);
+        for (let j = 0; j < _eMetadataKeys.length; j++) {
+          let eventsQueue = [];
+          let eventsQueueSize: number = 0;
+          const currentMetadataEventsQueue = processedEvents[_eMetadataKeys[j]];
 
-      for (let j = 0; j < _eMetadataKeys.length; j++) {
-        let eventsQueue = [];
-        let eventsQueueSize: number = 0;
-        const currentMetadataEventsQueue = processedEvents[_eMetadataKeys[j]];
+          while (currentMetadataEventsQueue.length > 0) {
+            const currentEvent = currentMetadataEventsQueue.shift() as StoredEventWithMetadata;
+            const currentEventSize = JSON.stringify(currentEvent).length;
 
-        while (currentMetadataEventsQueue.length > 0) {
-          const currentEvent = currentMetadataEventsQueue.shift() as StoredEventWithMetadata;
-          const currentEventSize = JSON.stringify(currentEvent).length;
+            // Case when the Queue size is already full.
+            if ((eventsQueueSize + currentEventSize) > MAX_QUEUE_BYTE_SIZE) {
+              await tryPostEventsBulk(eventsQueue, metadataToHeaders(currentEvent.m));
+              eventsQueue = [];
+              eventsQueueSize = 0;
+            }
+            eventsQueue.push(currentEvent.e);
+            eventsQueueSize += currentEventSize;
 
-          // Case when the Queue size is already full.
-          if ((eventsQueueSize + currentEventSize) > MAX_QUEUE_BYTE_SIZE) {
-            await tryPostEventsBulk(eventsQueue, metadataToHeaders(currentEvent.m));
-            eventsQueue = [];
-            eventsQueueSize = 0;
-          }
-          eventsQueue.push(currentEvent.e);
-          eventsQueueSize += currentEventSize;
-
-          // Case when there are no more events to process and the queue has events yet to be sent.
-          if (currentMetadataEventsQueue.length === 0 && eventsQueue.length > 0) {
-            await tryPostEventsBulk(eventsQueue, metadataToHeaders(currentEvent.m));
+            // Case when there are no more events to process and the queue has events yet to be sent.
+            if (currentMetadataEventsQueue.length === 0 && eventsQueue.length > 0) {
+              await tryPostEventsBulk(eventsQueue, metadataToHeaders(currentEvent.m));
+            }
           }
         }
-      }
-      return Promise.resolve(true);
-    })
-    .catch((e) => {
-      logger.error(`An error occurred when processing Events: ${e}`);
-      return false;
-    });
+        const count = await eventsCache.count();
+        if (count !== 0) processEventsBatch();
+        return Promise.resolve(true);
+      })
+      .catch((e) => {
+        logger.error(`An error occurred when processing Events: ${e}`);
+        return false;
+      });
+  }
+
+  return (batchSize?: number) => processEventsBatch(batchSize);
 }
