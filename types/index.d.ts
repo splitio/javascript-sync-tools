d14bcfafaf734c6b3f1fce79bc384190d4cf3bad
Emiliano Sanchez <emiliano.sanchez@split.io>
2025-05-20 10:59:37 -0300

    Apply TSDoc linter in types folder

--- types/index.d.ts
+++ types/index.d.ts
@@ -14,14 +14,13 @@ declare module JsSyncTools {
   export class Synchronizer {
     /**
      * Creates a new Synchronizer instance
-     * @param config The synchronizer config object
+     * @param config - The synchronizer config object
      */
     constructor(config: ISynchronizerSettings);
     /**
      * Execute synchronization
-     * @param cb Optional error-first callback to be invoked when the synchronization ends. The callback will be invoked with an error as first argument if the synchronization fails.
-     * @return {Promise<boolean>} A promise that resolves when the operation ends,
-     * with a boolean indicating if operation succeeded or not. The promise never rejects.
+     * @param cb - Optional error-first callback to be invoked when the synchronization ends. The callback will be invoked with an error as first argument if the synchronization fails.
+     * @returns A promise that resolves when the operation ends, with a boolean indicating if operation succeeded or not. The promise never rejects.
      */
     execute(cb?: (err?: Error) => void): Promise<boolean>;
     // @TODO expose settings eventually
@@ -30,8 +29,6 @@ declare module JsSyncTools {
 
   /**
    * Log levels.
-   *
-   * @typedef {string} LogLevel
    */
   type LogLevel = 'DEBUG' | 'INFO' | 'WARN' | 'ERROR' | 'NONE';
   /**
@@ -41,30 +38,25 @@ declare module JsSyncTools {
     /**
      * String property to override the base URL where the synchronizer will get feature flagging related data like a feature flag rollout plan or segments information.
      *
-     * @property {string} sdk
-     * @default 'https://sdk.split.io/api'
+     * @defaultValue `'https://sdk.split.io/api'`
      */
     sdk?: string
     /**
      * String property to override the base URL where the synchronizer will post impressions and events.
      *
-     * @property {string} events
-     * @default 'https://events.split.io/api'
+     * @defaultValue `'https://events.split.io/api'`
      */
     events?: string,
     /**
      * String property to override the base URL where the synchronizer will post telemetry data.
      *
-     * @property {string} telemetry
-     * @default 'https://telemetry.split.io/api'
+     * @defaultValue `'https://telemetry.split.io/api'`
      */
     telemetry?: string
   };
 
   /**
    * SplitFilter type.
-   *
-   * @typedef {string} SplitFilterType
    */
   type SplitFilterType = 'byName' | 'bySet';
   /**
@@ -73,73 +65,56 @@ declare module JsSyncTools {
   interface SplitFilter {
     /**
      * Type of the filter.
-     *
-     * @property {SplitFilterType} type
      */
     type: SplitFilterType,
     /**
      * List of values: feature flag names for 'byName' filter type, and feature flag name prefixes for 'byPrefix' type.
-     *
-     * @property {string[]} values
      */
     values: string[],
   }
   /**
    * ImpressionsMode type.
-   *
-   * @typedef {string} ImpressionsMode
    */
   type ImpressionsMode = 'OPTIMIZED' | 'DEBUG';
   /**
    * Settings interface for Synchronizer instances.
    *
-   * @interface ISynchronizerSettings
    * @see {@link https://help.split.io/hc/en-us/articles/4421513571469-Split-JavaScript-synchronizer-tools#configuration}
    */
   interface ISynchronizerSettings {
     /**
      * Core settings.
-     *
-     * @property {Object} core
      */
     core: {
       /**
-       * Your SDK key. More information: @see {@link https://help.split.io/hc/en-us/articles/360019916211-API-keys}
+       * Your SDK key.
        *
-       * @property {string} authorizationKey
+       * @see {@link https://help.split.io/hc/en-us/articles/360019916211-API-keys}
        */
       authorizationKey: string
     }
     /**
      * Defines which kind of storage we should instantiate.
-     *
-     * @property {Object} storage
      */
     storage: {
       /**
        * Storage type. The only possible value is "PLUGGABLE", which is the default.
-       * @property {'PLUGGABLE'} type
        */
       type?: 'PLUGGABLE',
       /**
        * A valid storage instance.
-       *
-       * @property {Object} wrapper
        */
       wrapper: Object
       /**
        * Optional prefix added to the storage keys to prevent any kind of data collision between SDK versions.
        *
-       * @property {string} prefix
-       * @default 'SPLITIO'
+       * @defaultValue `'SPLITIO'`
        */
       prefix?: string
     }
     /**
      * List of URLs that the Synchronizer will use as base for it's synchronization functionalities.
      * Do not change these settings unless you're working an advanced use case, like connecting to a proxy.
-     *
-     * @property {Object} urls
      */
     urls?: UrlSettings
     /**
@@ -150,30 +125,29 @@ declare module JsSyncTools {
      * config.debug = true
      * config.debug = 'WARN'
      * ```
-     * @property {boolean | LogLevel | ILogger} debug
-     * @default false
+     * @defaultValue `false`
      */
     debug?: boolean | LogLevel
     /**
      * Synchronization settings.
-     * @property {Object} sync
      */
     sync?: {
       /**
        * List of feature flag filters. These filters are used to fetch a subset of the feature flag definitions in your environment.
        *
        * Example:
-       *  `splitFilter: [
-       *    { type: 'byName', values: ['my_feature_flag_1', 'my_feature_flag_2'] }, // will fetch feature flags named 'my_feature_flag_1' and 'my_feature_flag_2'
-       *  ]`
-       * @property {SplitFilter[]} splitFilters
+       * ```
+       * splitFilter: [
+       *   { type: 'byName', values: ['my_feature_flag_1', 'my_feature_flag_2'] }, // will fetch feature flags named 'my_feature_flag_1' and 'my_feature_flag_2'
+       * ]
+       * ```
        */
       splitFilters?: SplitFilter[]
       /**
        * Feature Flag Spec version. Option to determine which version of the feature flag definitions are fetched and stored.
        * Possible values are '1.0' and '1.1'.
        *
-       * @default '1.1'
+       * @defaultValue `'1.1'`
        */
       flagSpecVersion?: '1.0' | '1.1'
       /**
@@ -183,8 +157,7 @@ declare module JsSyncTools {
        * - DEBUG: will send all the impressions generated (recommended only for debugging purposes).
        * - OPTIMIZED: will send unique impressions to Split Servers avoiding a considerable amount of traffic that duplicated impressions could generate.
        *
-       * @property {String} impressionsMode
-       * @default 'OPTIMIZED'
+       * @defaultValue `'OPTIMIZED'`
        */
       impressionsMode?: ImpressionsMode
       /**
@@ -237,33 +210,28 @@ declare module JsSyncTools {
          *
          * @see {@link https://nodejs.org/api/https.html#class-httpsagent}
          *
-         * @property {http.Agent | https.Agent} agent
-         * @default undefined
+         * @defaultValue `undefined`
          */
         agent?: RequestOptions['agent']
       },
     }
     /**
      * Scheduler settings.
-     * @property {Object} scheduler
      */
     scheduler?: {
       /**
        * Maximum number of impressions to send per POST request.
-       * @property {number} impressionsPerPost
-       * @default 1000
+       * @defaultValue `1000`
        */
       impressionsPerPost?: number
       /**
        * Maximum number of events to send per POST request.
-       * @property {number} eventsPerPost
-       * @default 1000
+       * @defaultValue `1000`
        */
       eventsPerPost?: number
       /**
        * Maximum number of retry attempts for posting impressions and events.
-       * @property {number} maxRetries
-       * @default 3
+       * @defaultValue `3`
        */
       maxRetries?: number
     }
